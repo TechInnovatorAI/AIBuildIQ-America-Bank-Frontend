d470717090241a832d16af2ec288c8dd80e7a4b4
TechInnovatorAI <masatotanaka0219@gmail.com>
2024-10-17 09:01:58 -0700

    first

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+import Hero from "@/components/landing/home/Hero";
+import Hero2 from "@/components/landing/home/Hero2";
+import Innovation from "@/components/landing/home/Innovation";
+import Services from "@/components/landing/home/Services";
+import Services2 from "@/components/landing/home/Services2";
+import Product from "@/components/landing/home/Product";
+import Product2 from "@/components/landing/home/Product2";
+import Product3 from "@/components/landing/home/Product3";
+import Product4 from "@/components/landing/home/Product4";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Hero2 />
+      <Innovation />
+      <Services />
+      <Services2 />
+      <Product />
+      <Product2 />
+      <Product3 />
+      <Product4 />
+    </>
+  );
+}
